75016efcbfefac3a993d5e0f5fb98310fe8ae8b2
tamal <tamal@appscode.com>
2017-06-11 20:14:35 -0700

    added-all

--- hack/gendocs/main.go
+++ hack/gendocs/main.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"github.com/k8sdb/cli/pkg/cmd"
+
 	"github.com/appscode/go/runtime"
+	"github.com/k8sdb/cli/pkg/cmd"
 	"github.com/spf13/cobra/doc"
 )
 
@@ -13,7 +14,7 @@ import (
 func main() {
 	rootCmd := cmd.NewKubedbCommand(os.Stdin, os.Stdout, os.Stderr, "0.0.0")
 	dir := runtime.GOPath() + "/src/github.com/k8sdb/cli/docs/user-guide/reference"
-	fmt.Printf("Generating cli markdown tree in %v\n", dir)
+	fmt.Printf("Generating cli markdown tree in: %v\n", dir)
 	err := os.MkdirAll(dir, 0755)
 	if err != nil {
 		log.Fatal(err)
