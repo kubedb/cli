96d56f5cf40ef3e8ee392c71d84f17767e19a1c8
tamal <tamal@appscode.com>
2017-06-11 20:13:27 -0700

    added-all

--- hack/gendocs/make.sh
+++ hack/gendocs/make.sh
@@ -2,4 +2,7 @@
 
 pushd $GOPATH/src/github.com/k8sdb/cli/hack/gendocs
 go run main.go
+
+cd $GOPATH/src/github.com/k8sdb/cli/docs/user-guide/reference
+sed -i 's/######\ Auto\ generated\ by.*//g' *
 popd
