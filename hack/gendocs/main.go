b1efdaeb57e364748e67f0a17d331569af2d8b1e
tamal <tamal@appscode.com>
2017-06-15 02:01:25 -0700

    Use reduced pkg depth in gendocs

--- hack/gendocs/main.go
+++ hack/gendocs/main.go
@@ -6,13 +6,13 @@ import (
 	"os"
 
 	"github.com/appscode/go/runtime"
-	"github.com/k8sdb/cli/pkg/cmd"
+	"github.com/k8sdb/cli/pkg"
 	"github.com/spf13/cobra/doc"
 )
 
 // ref: https://github.com/spf13/cobra/blob/master/doc/md_docs.md
 func main() {
-	rootCmd := cmd.NewKubedbCommand(os.Stdin, os.Stdout, os.Stderr, "0.0.0")
+	rootCmd := pkg.NewKubedbCommand(os.Stdin, os.Stdout, os.Stderr, "0.0.0")
 	dir := runtime.GOPath() + "/src/github.com/k8sdb/cli/docs/user-guide/reference"
 	fmt.Printf("Generating cli markdown tree in: %v\n", dir)
 	err := os.MkdirAll(dir, 0755)
