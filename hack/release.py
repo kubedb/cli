68ad6dc757f012610d6b6b995ca34cb58fc10ef9
tamal <tamal@appscode.com>
2017-07-07 21:03:48 -0700

    Avoid new commits if tag already present.

--- hack/release.py
+++ hack/release.py
@@ -78,9 +78,12 @@ def git_checkout(branch, cwd=libbuild.REPO_ROOT):
     call('git checkout -f -B {0} origin/{0}'.format(branch), cwd=cwd)
 
 
-def git_requires_commit(cwd=libbuild.REPO_ROOT):
+def git_requires_commit(tag, cwd=libbuild.REPO_ROOT):
+    status = call('git rev-parse {0} >/dev/null 2>&1'.format(tag), eoe=False, cwd=cwd)
+    if status == 0:
+        return False
     changed_files = check_output('git diff --name-only', cwd=cwd).strip().split('\n')
-    return Counter(changed_files) != Counter(['glide.yaml', 'glide.lock'])
+    return Counter(changed_files) != Counter(['glide.lock'])
 
 
 def glide_mod(glide_config, changes):
@@ -121,7 +124,7 @@ class Kitten(object):
         print('----------------------------------------------------------------------------------------')
         git_checkout('master', cwd=repo)
         call('glide slow', cwd=repo)
-        if git_requires_commit(cwd=repo):
+        if git_requires_commit(self.tag, cwd=repo):
             call('./hack/make.py', cwd=repo)
             call('git commit -a -m "Prepare release {0}"'.format(self.tag), cwd=repo, eoe=False)
             call('git push origin master', cwd=repo)
@@ -141,7 +144,7 @@ class Kitten(object):
             glide_mod(glide_config, self.rel_deps)
             glide_write(glide_file, glide_config)
             call('glide slow', cwd=repo)
-            if git_requires_commit(cwd=repo):
+            if git_requires_commit(self.tag, cwd=repo):
                 call('./hack/make.py', cwd=repo)
                 call('git commit -a -m "Prepare release {0}"'.format(self.tag), cwd=repo, eoe=False)
                 call('git push origin master', cwd=repo)
@@ -170,7 +173,7 @@ class Kitten(object):
             glide_mod(glide_config, self.rel_deps)
             glide_write(glide_file, glide_config)
             call('glide slow', cwd=repo)
-            if git_requires_commit(cwd=repo):
+            if git_requires_commit(self.tag, cwd=repo):
                 call('./hack/make.py', cwd=repo)
                 call('git commit -a -m "Prepare release {0}"'.format(self.tag), cwd=repo, eoe=False)
                 call('git push origin master', cwd=repo)
