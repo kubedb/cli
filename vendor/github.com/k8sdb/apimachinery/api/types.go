85e6cf52c7f1c003b7b8a3e5ae36924e7a128864
tamal <tamal@appscode.com>
2017-07-07 21:36:18 -0700

    Prepare release 0.3.0

--- vendor/github.com/k8sdb/apimachinery/api/types.go
+++ vendor/github.com/k8sdb/apimachinery/api/types.go
@@ -91,9 +91,8 @@ type S3Spec struct {
 }
 
 type GCSSpec struct {
-	Location string `json:"location,omitempty"`
-	Bucket   string `json:"bucket,omiempty"`
-	Prefix   string `json:"prefix,omitempty"`
+	Bucket string `json:"bucket,omiempty"`
+	Prefix string `json:"prefix,omitempty"`
 }
 
 type AzureSpec struct {
