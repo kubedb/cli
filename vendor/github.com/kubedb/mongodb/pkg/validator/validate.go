cee38fce5261b54b76ad032e0e2da3de7daa31e5
tamal <tamal@appscode.com>
2018-02-14 21:49:09 -0800

    Fix pluralization of Elasticsearch

--- vendor/github.com/kubedb/mongodb/pkg/validator/validate.go
+++ vendor/github.com/kubedb/mongodb/pkg/validator/validate.go
@@ -4,23 +4,24 @@ import (
 	"fmt"
 
 	api "github.com/kubedb/apimachinery/apis/kubedb/v1alpha1"
-	adr "github.com/kubedb/apimachinery/pkg/docker"
 	amv "github.com/kubedb/apimachinery/pkg/validator"
-	dr "github.com/kubedb/mongodb/pkg/docker"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/kubernetes"
 )
 
-func ValidateMongoDB(client kubernetes.Interface, mongodb *api.MongoDB, docker *dr.Docker) error {
+var (
+	mongodbVersions = sets.NewString("3.4", "3.6")
+)
+
+func ValidateMongoDB(client kubernetes.Interface, mongodb *api.MongoDB) error {
 	if mongodb.Spec.Version == "" {
 		return fmt.Errorf(`object 'Version' is missing in '%v'`, mongodb.Spec)
 	}
 
-	if docker != nil {
-		// Set Database Image version
-		if err := adr.CheckDockerImageVersion(docker.GetImage(mongodb), string(mongodb.Spec.Version)); err != nil {
-			return fmt.Errorf(`image %s not found`, docker.GetImageWithTag(mongodb))
-		}
+	// check MongoDB version validation
+	if !mongodbVersions.Has(string(mongodb.Spec.Version)) {
+		return fmt.Errorf(`KubeDB doesn't support MongoDB version: %s`, string(mongodb.Spec.Version))
 	}
 
 	if mongodb.Spec.Storage != nil {
@@ -49,7 +50,6 @@ func ValidateMongoDB(client kubernetes.Interface, mongodb *api.MongoDB, docker *
 		if err := amv.ValidateMonitorSpec(monitorSpec); err != nil {
 			return err
 		}
-
 	}
 	return nil
 }
