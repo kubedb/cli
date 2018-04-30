ba6c53f73b7af2af8d3c283ad6bcf11504f0caa2
Tamal Saha <tamal@appscode.com>
2018-04-30 13:51:11 -0700

    Improve installer (#212)

--- hack/deploy/kubedb.sh
+++ hack/deploy/kubedb.sh
@@ -2,6 +2,7 @@
 set -eou pipefail
 
 crds=(elasticsearches memcacheds mongodbs mysqls postgreses redises snapshots dormantdatabases)
+apiServices=(v1alpha1.validators v1alpha1.mutators)
 
 echo "checking kubeconfig context"
 kubectl config current-context || { echo "Set a context (kubectl use-context <context>) out of the following:"; echo; kubectl config get-contexts; exit 1; }
@@ -13,6 +14,46 @@ function cleanup {
 }
 trap cleanup EXIT
 
+# ref: https://github.com/appscodelabs/libbuild/blob/master/common/lib.sh#L55
+inside_git_repo() {
+    git rev-parse --is-inside-work-tree > /dev/null 2>&1
+    inside_git=$?
+    if [ "$inside_git" -ne 0 ]; then
+        echo "Not inside a git repository"
+        exit 1
+    fi
+}
+
+detect_tag() {
+    inside_git_repo
+
+    # http://stackoverflow.com/a/1404862/3476121
+    git_tag=$(git describe --exact-match --abbrev=0 2>/dev/null || echo '')
+
+    commit_hash=$(git rev-parse --verify HEAD)
+    git_branch=$(git rev-parse --abbrev-ref HEAD)
+    commit_timestamp=$(git show -s --format=%ct)
+
+    if [ "$git_tag" != '' ]; then
+        TAG=$git_tag
+        TAG_STRATEGY='git_tag'
+    elif [ "$git_branch" != 'master' ] && [ "$git_branch" != 'HEAD' ] && [[ "$git_branch" != release-* ]]; then
+        TAG=$git_branch
+        TAG_STRATEGY='git_branch'
+    else
+        hash_ver=$(git describe --tags --always --dirty)
+        TAG="${hash_ver}"
+        TAG_STRATEGY='commit_hash'
+    fi
+
+    export TAG
+    export TAG_STRATEGY
+    export git_tag
+    export git_branch
+    export commit_hash
+    export commit_timestamp
+}
+
 # https://stackoverflow.com/a/677212/244009
 if [ -x "$(command -v onessl)" ]; then
     export ONESSL=onessl
@@ -20,19 +61,19 @@ else
     # ref: https://stackoverflow.com/a/27776822/244009
     case "$(uname -s)" in
         Darwin)
-            curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.1.0/onessl-darwin-amd64
+            curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.3.0/onessl-darwin-amd64
             chmod +x onessl
             export ONESSL=./onessl
             ;;
 
         Linux)
-            curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.1.0/onessl-linux-amd64
+            curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.3.0/onessl-linux-amd64
             chmod +x onessl
             export ONESSL=./onessl
             ;;
 
         CYGWIN*|MINGW32*|MSYS*)
-            curl -fsSL -o onessl.exe https://github.com/kubepack/onessl/releases/download/0.1.0/onessl-windows-amd64.exe
+            curl -fsSL -o onessl.exe https://github.com/kubepack/onessl/releases/download/0.3.0/onessl-windows-amd64.exe
             chmod +x onessl.exe
             export ONESSL=./onessl.exe
             ;;
@@ -50,20 +91,27 @@ export KUBEDB_NAMESPACE=kube-system
 export KUBEDB_SERVICE_ACCOUNT=kubedb-operator
 export KUBEDB_ENABLE_RBAC=true
 export KUBEDB_RUN_ON_MASTER=0
-export KUBEDB_ENABLE_ADMISSION_WEBHOOK=false
+export KUBEDB_ENABLE_VALIDATING_WEBHOOK=false
+export KUBEDB_ENABLE_MUTATING_WEBHOOK=false
 export KUBEDB_DOCKER_REGISTRY=kubedb
+export KUBEDB_OPERATOR_TAG=0.8.0-beta.2
 export KUBEDB_IMAGE_PULL_SECRET=
+export KUBEDB_IMAGE_PULL_POLICY=IfNotPresent
+export KUBEDB_ENABLE_ANALYTICS=true
 export KUBEDB_UNINSTALL=0
 export KUBEDB_PURGE=0
 
 export APPSCODE_ENV=${APPSCODE_ENV:-prod}
 export SCRIPT_LOCATION="curl -fsSL https://raw.githubusercontent.com/kubedb/cli/0.8.0-beta.2/"
 if [ "$APPSCODE_ENV" = "dev" ]; then
+    detect_tag
     export SCRIPT_LOCATION="cat "
+    export KUBEDB_OPERATOR_TAG=$TAG
+    export KUBEDB_IMAGE_PULL_POLICY=Always
 fi
 
 KUBE_APISERVER_VERSION=$(kubectl version -o=json | $ONESSL jsonpath '{.serverVersion.gitVersion}')
-$ONESSL semver --check='<1.9.0' $KUBE_APISERVER_VERSION || { export KUBEDB_ENABLE_ADMISSION_WEBHOOK=true; }
+$ONESSL semver --check='<1.9.0' $KUBE_APISERVER_VERSION || { export KUBEDB_ENABLE_VALIDATING_WEBHOOK=true; export KUBEDB_ENABLE_MUTATING_WEBHOOK=true; }
 
 show_help() {
     echo "kubedb.sh - install kubedb operator"
@@ -74,12 +122,14 @@ show_help() {
     echo "-h, --help                         show brief help"
     echo "-n, --namespace=NAMESPACE          specify namespace (default: kube-system)"
     echo "    --rbac                         create RBAC roles and bindings (default: true)"
-    echo "    --docker-registry              docker registry used to pull kubedb images (default: appscode)"
-    echo "    --image-pull-secret            name of secret used to pull kubedb operator images"
-    echo "    --run-on-master                run kubedb operator on master"
-    echo "    --enable-admission-webhook     configure admission webhook for kubedb CRDs"
-    echo "    --uninstall                    uninstall kubedb"
-    echo "    --purge                        purges kubedb crd objects and crds"
+    echo "    --docker-registry              docker registry used to pull KubeDB images (default: appscode)"
+    echo "    --image-pull-secret            name of secret used to pull KubeDB operator images"
+    echo "    --run-on-master                run KubeDB operator on master"
+    echo "    --enable-validating-webhook    enable/disable validating webhooks for KubeDB CRDs"
+    echo "    --enable-mutating-webhook      enable/disable mutating webhooks for KubeDB CRDs"
+    echo "    --enable-analytics             send usage events to Google Analytics (default: true)"
+    echo "    --uninstall                    uninstall KubeDB"
+    echo "    --purge                        purges KubeDB crd objects and crds"
 }
 
 while test $# -gt 0; do
@@ -111,12 +161,24 @@ while test $# -gt 0; do
             export KUBEDB_IMAGE_PULL_SECRET="name: '$secret'"
             shift
             ;;
-        --enable-admission-webhook*)
+        --enable-validating-webhook*)
             val=`echo $1 | sed -e 's/^[^=]*=//g'`
             if [ "$val" = "false" ]; then
-                export KUBEDB_ENABLE_ADMISSION_WEBHOOK=false
-            else
-                export KUBEDB_ENABLE_ADMISSION_WEBHOOK=true
+                export KUBEDB_ENABLE_VALIDATING_WEBHOOK=false
+            fi
+            shift
+            ;;
+        --enable-mutating-webhook*)
+            val=`echo $1 | sed -e 's/^[^=]*=//g'`
+            if [ "$val" = "false" ]; then
+                export KUBEDB_ENABLE_MUTATING_WEBHOOK=false
+            fi
+            shift
+            ;;
+        --enable-analytics*)
+            val=`echo $1 | sed -e 's/^[^=]*=//g'`
+            if [ "$val" = "false" ]; then
+                export KUBEDB_ENABLE_ANALYTICS=false
             fi
             shift
             ;;
@@ -148,31 +210,6 @@ while test $# -gt 0; do
 done
 
 if [ "$KUBEDB_UNINSTALL" -eq 1 ]; then
-    # delete webhooks and apiservices
-    kubectl delete validatingwebhookconfiguration -l app=kubedb || true
-    kubectl delete mutatingwebhookconfiguration -l app=kubedb || true
-    kubectl delete apiservice -l app=kubedb
-    # delete kubedb operator
-    kubectl delete deployment -l app=kubedb --namespace $KUBEDB_NAMESPACE
-    kubectl delete service -l app=kubedb --namespace $KUBEDB_NAMESPACE
-    kubectl delete secret -l app=kubedb --namespace $KUBEDB_NAMESPACE
-    # delete RBAC objects, if --rbac flag was used.
-    kubectl delete serviceaccount -l app=kubedb --namespace $KUBEDB_NAMESPACE
-    kubectl delete clusterrolebindings -l app=kubedb
-    kubectl delete clusterrole -l app=kubedb
-    kubectl delete rolebindings -l app=kubedb --namespace $KUBEDB_NAMESPACE
-    kubectl delete role -l app=kubedb --namespace $KUBEDB_NAMESPACE
-
-    echo "waiting for kubedb operator pod to stop running"
-    for (( ; ; )); do
-       pods=($(kubectl get pods --all-namespaces -l app=kubedb -o jsonpath='{range .items[*]}{.metadata.name} {end}'))
-       total=${#pods[*]}
-        if [ $total -eq 0 ] ; then
-            break
-        fi
-       sleep 2
-    done
-
     # https://github.com/kubernetes/kubernetes/issues/60538
     if [ "$KUBEDB_PURGE" -eq 1 ]; then
         for crd in "${crds[@]}"; do
@@ -198,7 +235,35 @@ if [ "$KUBEDB_UNINSTALL" -eq 1 ]; then
             # delete crd
             kubectl delete crd ${crd}.kubedb.com || true
         done
+
+        echo "waiting 5 seconds ..."
+        sleep 5;
     fi
+
+    # delete webhooks and apiservices
+    kubectl delete validatingwebhookconfiguration -l app=kubedb || true
+    kubectl delete mutatingwebhookconfiguration -l app=kubedb || true
+    kubectl delete apiservice -l app=kubedb
+    # delete kubedb operator
+    kubectl delete deployment -l app=kubedb --namespace $KUBEDB_NAMESPACE
+    kubectl delete service -l app=kubedb --namespace $KUBEDB_NAMESPACE
+    kubectl delete secret -l app=kubedb --namespace $KUBEDB_NAMESPACE
+    # delete RBAC objects, if --rbac flag was used.
+    kubectl delete serviceaccount -l app=kubedb --namespace $KUBEDB_NAMESPACE
+    kubectl delete clusterrolebindings -l app=kubedb
+    kubectl delete clusterrole -l app=kubedb
+    kubectl delete rolebindings -l app=kubedb --namespace $KUBEDB_NAMESPACE
+    kubectl delete role -l app=kubedb --namespace $KUBEDB_NAMESPACE
+
+    echo "waiting for kubedb operator pod to stop running"
+    for (( ; ; )); do
+       pods=($(kubectl get pods --all-namespaces -l app=kubedb -o jsonpath='{range .items[*]}{.metadata.name} {end}'))
+       total=${#pods[*]}
+        if [ $total -eq 0 ] ; then
+            break
+        fi
+       sleep 2
+    done
 
     echo
     echo "Successfully uninstalled KubeDB!"
@@ -208,6 +273,12 @@ fi
 echo "checking whether extended apiserver feature is enabled"
 $ONESSL has-keys configmap --namespace=kube-system --keys=requestheader-client-ca-file extension-apiserver-authentication || { echo "Set --requestheader-client-ca-file flag on Kubernetes apiserver"; exit 1; }
 echo ""
+
+export KUBE_CA=
+if [ "$KUBEDB_ENABLE_VALIDATING_WEBHOOK" = true ] || [ "$KUBEDB_ENABLE_MUTATING_WEBHOOK" = true ]; then
+    $ONESSL get kube-ca >/dev/null 2>&1 || { echo "Admission webhooks can't be used when kube apiserver is accesible without verifying its TLS certificate (insecure-skip-tls-verify : true)."; echo; exit 1; }
+    export KUBE_CA=$($ONESSL get kube-ca | $ONESSL base64)
+fi
 
 env | sort | grep KUBEDB*
 echo ""
@@ -220,7 +291,6 @@ $ONESSL create server-cert server --domains=kubedb-operator.$KUBEDB_NAMESPACE.sv
 export SERVICE_SERVING_CERT_CA=$(cat ca.crt | $ONESSL base64)
 export TLS_SERVING_CERT=$(cat server.crt | $ONESSL base64)
 export TLS_SERVING_KEY=$(cat server.key | $ONESSL base64)
-export KUBE_CA=$($ONESSL get kube-ca | $ONESSL base64)
 
 ${SCRIPT_LOCATION}hack/deploy/operator.yaml | $ONESSL envsubst | kubectl apply -f -
 
@@ -237,8 +307,12 @@ if [ "$KUBEDB_RUN_ON_MASTER" -eq 1 ]; then
       --patch="$(${SCRIPT_LOCATION}hack/deploy/run-on-master.yaml)"
 fi
 
-if [ "$KUBEDB_ENABLE_ADMISSION_WEBHOOK" = true ]; then
-    ${SCRIPT_LOCATION}hack/deploy/admission.yaml | $ONESSL envsubst | kubectl apply -f -
+if [ "$KUBEDB_ENABLE_VALIDATING_WEBHOOK" = true ]; then
+    ${SCRIPT_LOCATION}hack/deploy/validating-webhook.yaml | $ONESSL envsubst | kubectl apply -f -
+fi
+
+if [ "$KUBEDB_ENABLE_MUTATING_WEBHOOK" = true ]; then
+    ${SCRIPT_LOCATION}hack/deploy/mutating-webhook.yaml | $ONESSL envsubst | kubectl apply -f -
 fi
 
 echo
@@ -246,7 +320,9 @@ echo "waiting until kubedb operator deployment is ready"
 $ONESSL wait-until-ready deployment kubedb-operator --namespace $KUBEDB_NAMESPACE || { echo "KubeDB operator deployment failed to be ready"; exit 1; }
 
 echo "waiting until kubedb apiservice is available"
-$ONESSL wait-until-ready apiservice v1alpha1.admission.kubedb.com || { echo "KubeDB apiservice failed to be ready"; exit 1; }
+for api in "${apiServices[@]}"; do
+    $ONESSL wait-until-ready apiservice ${api}.kubedb.com || { echo "KubeDB apiservice $api failed to be ready"; exit 1; }
+done
 
 echo "waiting until kubedb crds are ready"
 for crd in "${crds[@]}"; do
@@ -254,4 +330,4 @@ for crd in "${crds[@]}"; do
 done
 
 echo
-echo "Successfully installed KubeDB operator!"
+echo "Successfully installed KubeDB operator in $KUBEDB_NAMESPACE namespace!"
