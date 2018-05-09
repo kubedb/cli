599fda0e6423c4f6bb1c35875e0302e66de3c351
the-redback <maruf@appscode.com>
2018-05-09 10:59:49 +0600

    Skip validation while creating an CRD object

--- pkg/cmds/create.go
+++ pkg/cmds/create.go
@@ -6,12 +6,9 @@ import (
 	"io"
 
 	api "github.com/kubedb/apimachinery/apis/kubedb/v1alpha1"
-	cs "github.com/kubedb/apimachinery/client/clientset/versioned/typed/kubedb/v1alpha1"
 	"github.com/kubedb/cli/pkg/kube"
 	"github.com/kubedb/cli/pkg/util"
-	"github.com/kubedb/cli/pkg/validator"
 	"github.com/spf13/cobra"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/kubernetes/pkg/kubectl/cmd/templates"
 	cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
 	"k8s.io/kubernetes/pkg/kubectl/resource"
@@ -71,20 +68,6 @@ func RunCreate(f cmdutil.Factory, cmd *cobra.Command, out io.Writer, options *re
 		return err
 	}
 
-	config, err := f.ClientConfig()
-	if err != nil {
-		return err
-	}
-	client, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return err
-	}
-
-	extClient, err := cs.NewForConfig(config)
-	if err != nil {
-		return err
-	}
-
 	infoList := make([]*resource.Info, 0)
 	err = r.Visit(func(info *resource.Info, err error) error {
 		if err != nil {
@@ -98,11 +81,6 @@ func RunCreate(f cmdutil.Factory, cmd *cobra.Command, out io.Writer, options *re
 
 		if kind == api.ResourceKindDormantDatabase {
 			return fmt.Errorf(`resource type "%v" doesn't support create operation`, kind)
-		}
-
-		fmt.Println(fmt.Sprintf(`validating "%v"`, info.Source))
-		if err := validator.Validate(client, extClient, info); err != nil {
-			return cmdutil.AddSourceToErr("validating", info.Source, err)
 		}
 
 		infoList = append(infoList, info)
--- pkg/validator/validate.go
+++ pkg/validator/validate.go
@@ -5,72 +5,9 @@ import (
 
 	"github.com/ghodss/yaml"
 	tapi "github.com/kubedb/apimachinery/apis/kubedb/v1alpha1"
-	cs "github.com/kubedb/apimachinery/client/clientset/versioned/typed/kubedb/v1alpha1"
-	amv "github.com/kubedb/apimachinery/pkg/validator"
 	"github.com/kubedb/cli/pkg/encoder"
-	esv "github.com/kubedb/elasticsearch/pkg/validator"
-	memv "github.com/kubedb/memcached/pkg/admission"
-	mgv "github.com/kubedb/mongodb/pkg/admission"
-	msv "github.com/kubedb/mysql/pkg/admission"
-	pgv "github.com/kubedb/postgres/pkg/admission"
-	rdv "github.com/kubedb/redis/pkg/admission"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/kubernetes/pkg/kubectl/resource"
 )
-
-func Validate(client kubernetes.Interface, extClient cs.KubedbV1alpha1Interface, info *resource.Info) error {
-	objByte, err := encoder.Encode(info.Object)
-	if err != nil {
-		return err
-	}
-
-	kind := info.Object.GetObjectKind().GroupVersionKind().Kind
-	switch kind {
-	case tapi.ResourceKindElasticsearch:
-		var elasticsearch *tapi.Elasticsearch
-		if err := yaml.Unmarshal(objByte, &elasticsearch); err != nil {
-			return err
-		}
-		return esv.ValidateElasticsearch(client, extClient, elasticsearch)
-	case tapi.ResourceKindPostgres:
-		var postgres *tapi.Postgres
-		if err := yaml.Unmarshal(objByte, &postgres); err != nil {
-			return err
-		}
-		return pgv.ValidatePostgres(client, extClient, postgres)
-	case tapi.ResourceKindMySQL:
-		var mysql *tapi.MySQL
-		if err := yaml.Unmarshal(objByte, &mysql); err != nil {
-			return err
-		}
-		return msv.ValidateMySQL(client, extClient, mysql)
-	case tapi.ResourceKindMongoDB:
-		var mongodb *tapi.MongoDB
-		if err := yaml.Unmarshal(objByte, &mongodb); err != nil {
-			return err
-		}
-		return mgv.ValidateMongoDB(client, extClient, mongodb)
-	case tapi.ResourceKindRedis:
-		var redis *tapi.Redis
-		if err := yaml.Unmarshal(objByte, &redis); err != nil {
-			return err
-		}
-		return rdv.ValidateRedis(client, extClient, redis)
-	case tapi.ResourceKindMemcached:
-		var memcached *tapi.Memcached
-		if err := yaml.Unmarshal(objByte, &memcached); err != nil {
-			return err
-		}
-		return memv.ValidateMemcached(client, extClient, memcached)
-	case tapi.ResourceKindSnapshot:
-		var snapshot *tapi.Snapshot
-		if err := yaml.Unmarshal(objByte, &snapshot); err != nil {
-			return err
-		}
-		return amv.ValidateSnapshotSpec(client, snapshot.Spec.SnapshotStorageSpec, info.Namespace)
-	}
-	return nil
-}
 
 func ValidateDeletion(info *resource.Info) error {
 	objByte, err := encoder.Encode(info.Object)
