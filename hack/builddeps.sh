206fbee1a53d497fa33552994baf858832fc6a71
Tamal Saha <tamal@appscode.com>
2018-09-10 23:47:28 -0700

    Update builddeps.sh

--- hack/builddeps.sh
+++ hack/builddeps.sh
@@ -7,3 +7,5 @@ pip install pyyaml
 go get -u golang.org/x/tools/cmd/goimports
 go get github.com/Masterminds/glide
 go get github.com/sgotti/glide-vc
+go get github.com/onsi/ginkgo/ginkgo
+go install github.com/onsi/ginkgo/ginkgo
