f66683e3fb392aafe3c40e857c8af3dcdb76bc17
Tamal Saha <tamal@appscode.com>
2018-12-02 09:22:15 -0800

    Prepare release 0.9.0-rc.1

--- vendor/github.com/kubedb/apimachinery/apis/authorization/v1alpha1/database_access_request_helpers.go
+++ vendor/github.com/kubedb/apimachinery/apis/authorization/v1alpha1/database_access_request_helpers.go
@@ -1,27 +1,17 @@
 package v1alpha1
 
 import (
-	"fmt"
-
 	crdutils "github.com/appscode/kutil/apiextensions/v1beta1"
 	"github.com/kubedb/apimachinery/apis"
 	apiextensions "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 )
 
-func (r PostgresRole) RoleName() string {
-	cluster := "-"
-	if r.ClusterName != "" {
-		cluster = r.ClusterName
-	}
-	return fmt.Sprintf("k8s.%s.%s.%s", cluster, r.Namespace, r.Name)
-}
-
-func (r PostgresRole) CustomResourceDefinition() *apiextensions.CustomResourceDefinition {
+func (d DatabaseAccessRequest) CustomResourceDefinition() *apiextensions.CustomResourceDefinition {
 	return crdutils.NewCustomResourceDefinition(crdutils.Config{
 		Group:         SchemeGroupVersion.Group,
-		Plural:        ResourcePostgresRoles,
-		Singular:      ResourcePostgresRole,
-		Kind:          ResourceKindPostgresRole,
+		Plural:        ResourceDatabaseAccessRequests,
+		Singular:      ResourceDatabaseAccessRequest,
+		Kind:          ResourceKindDatabaseAccessRequest,
 		Categories:    []string{"datastore", "kubedb", "appscode", "all"},
 		ResourceScope: string(apiextensions.NamespaceScoped),
 		Versions: []apiextensions.CustomResourceDefinitionVersion{
@@ -34,13 +24,13 @@ func (r PostgresRole) CustomResourceDefinition() *apiextensions.CustomResourceDe
 		Labels: crdutils.Labels{
 			LabelsMap: map[string]string{"app": "kubedb"},
 		},
-		SpecDefinitionName:      "github.com/kubedb/apimachinery/apis/authorization/v1alpha1.PostgresRole",
+		SpecDefinitionName:      "github.com/kubedb/apimachinery/apis/authorization/v1alpha1.DatabaseAccessRequest",
 		EnableValidation:        true,
 		GetOpenAPIDefinitions:   GetOpenAPIDefinitions,
 		EnableStatusSubresource: apis.EnableStatusSubresource,
 	})
 }
 
-func (r PostgresRole) IsValid() error {
+func (d DatabaseAccessRequest) IsValid() error {
 	return nil
 }
