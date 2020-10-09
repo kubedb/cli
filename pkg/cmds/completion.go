4e46c763cf90c7b631871faf399384616077bc46
Tamal Saha <tamal@appscode.com>
2020-10-08 18:25:12 -0700

    Add completion command (#507)
    ref: https://github.com/spf13/cobra/blob/master/shell_completions.md
    
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ pkg/cmds/completion.go
@@ -0,0 +1,78 @@
+/*
+Copyright AppsCode Inc. and Contributors
+
+Licensed under the AppsCode Community License 1.0.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://github.com/appscode/licenses/raw/1.0.0/AppsCode-Community-1.0.0.md
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmds
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func NewCmdCompletion() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "completion [bash|zsh|fish|powershell]",
+		Short: "Generate completion script",
+		Long: `To load completions:
+
+Bash:
+
+$ source <(kubectl-dba completion bash)
+
+# To load completions for each session, execute once:
+Linux:
+  $ kubectl-dba completion bash > /etc/bash_completion.d/kubectl-dba
+MacOS:
+  $ kubectl-dba completion bash > /usr/local/etc/bash_completion.d/kubectl-dba
+
+Zsh:
+
+# If shell completion is not already enabled in your environment you will need
+# to enable it.  You can execute the following once:
+
+$ echo "autoload -U compinit; compinit" >> ~/.zshrc
+
+# To load completions for each session, execute once:
+$ kubectl-dba completion zsh > "${fpath[1]}/_kubectl-dba"
+
+# You will need to start a new shell for this setup to take effect.
+
+Fish:
+
+$ kubectl-dba completion fish | source
+
+# To load completions for each session, execute once:
+$ kubectl-dba completion fish > ~/.config/fish/completions/kubectl-dba.fish
+`,
+		DisableFlagsInUseLine: true,
+		ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
+		Args:                  cobra.ExactValidArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			switch args[0] {
+			case "bash":
+				return cmd.Root().GenBashCompletion(os.Stdout)
+			case "zsh":
+				return cmd.Root().GenZshCompletion(os.Stdout)
+			case "fish":
+				return cmd.Root().GenFishCompletion(os.Stdout, true)
+			case "powershell":
+				return cmd.Root().GenPowerShellCompletion(os.Stdout)
+			}
+			return nil
+		},
+	}
+	return cmd
+}
