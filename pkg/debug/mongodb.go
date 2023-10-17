4afc09e227974fbe904632fe5f80f7e8d300515a
Arnob kumar saha <arnob@appscode.com>
2023-10-17 22:55:32 +0600

    Use OffshootLabels while listing pods
    Signed-off-by: Arnob kumar saha <arnob@appscode.com>

--- pkg/debug/mongodb.go
+++ pkg/debug/mongodb.go
@@ -154,7 +154,7 @@ func (opts *mongodbOpts) collectOperatorLogs() error {
 }
 
 func (opts *mongodbOpts) collectForAllDBPods() error {
-	dbLabels := labels.SelectorFromSet(opts.db.GetLabels()).String()
+	dbLabels := labels.SelectorFromSet(opts.db.OffshootLabels()).String()
 	pods, err := opts.podClient.CoreV1().Pods(opts.db.Namespace).List(context.TODO(), metav1.ListOptions{
 		LabelSelector: dbLabels,
 	})
