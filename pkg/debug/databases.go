17b7c768875eb0316b558f1a99eb37e8e7fed559
Arnob Kumar Saha <arnob@appscode.com>
2026-05-22 13:03:30 +0600

    consts
    Signed-off-by: Arnob Kumar Saha <arnob@appscode.com>

--- pkg/debug/databases.go
+++ pkg/debug/databases.go
@@ -42,100 +42,136 @@ type dbEntry struct {
 var databases = []dbEntry{
 	// v1
 	{
-		use: "elasticsearch", aliases: []string{"es", "elasticsearches"}, short: "Debug helper for Elasticsearch database",
+		use: dbapi.ResourceSingularElasticsearch, aliases: []string{dbapi.ResourceCodeElasticsearch, dbapi.ResourcePluralElasticsearch}, short: "Debug helper for Elasticsearch database",
 		gvk: schema.GroupVersionKind{Group: dbapi.SchemeGroupVersion.Group, Version: dbapi.SchemeGroupVersion.Version, Kind: dbapi.ResourceKindElasticsearch},
 	},
 	{
-		use: "kafka", aliases: []string{"kf", "kafkas"}, short: "Debug helper for Kafka database",
+		use: dbapi.ResourceSingularKafka, aliases: []string{dbapi.ResourceCodeKafka, dbapi.ResourcePluralKafka}, short: "Debug helper for Kafka database",
 		gvk: schema.GroupVersionKind{Group: dbapi.SchemeGroupVersion.Group, Version: dbapi.SchemeGroupVersion.Version, Kind: dbapi.ResourceKindKafka},
 	},
 	{
-		use: "mariadb", aliases: []string{"md", "mariadbs"}, short: "Debug helper for MariaDB database",
+		use: dbapi.ResourceSingularMariaDB, aliases: []string{dbapi.ResourceCodeMariaDB, dbapi.ResourcePluralMariaDB}, short: "Debug helper for MariaDB database",
 		gvk: schema.GroupVersionKind{Group: dbapi.SchemeGroupVersion.Group, Version: dbapi.SchemeGroupVersion.Version, Kind: dbapi.ResourceKindMariaDB},
 	},
 	{
-		use: "memcached", aliases: []string{"mc", "memcacheds"}, short: "Debug helper for Memcached database",
+		use: dbapi.ResourceSingularMemcached, aliases: []string{dbapi.ResourceCodeMemcached, dbapi.ResourcePluralMemcached}, short: "Debug helper for Memcached database",
 		gvk: schema.GroupVersionKind{Group: dbapi.SchemeGroupVersion.Group, Version: dbapi.SchemeGroupVersion.Version, Kind: dbapi.ResourceKindMemcached},
 	},
 	{
-		use: "mongodb", aliases: []string{"mg", "mongodbs"}, short: "Debug helper for MongoDB database",
+		use: dbapi.ResourceSingularMongoDB, aliases: []string{dbapi.ResourceCodeMongoDB, dbapi.ResourcePluralMongoDB}, short: "Debug helper for MongoDB database",
 		gvk: schema.GroupVersionKind{Group: dbapi.SchemeGroupVersion.Group, Version: dbapi.SchemeGroupVersion.Version, Kind: dbapi.ResourceKindMongoDB},
 	},
 	{
-		use: "mysql", aliases: []string{"my", "mysqls"}, short: "Debug helper for MySQL database",
+		use: dbapi.ResourceSingularMySQL, aliases: []string{dbapi.ResourceCodeMySQL, dbapi.ResourcePluralMySQL}, short: "Debug helper for MySQL database",
 		gvk: schema.GroupVersionKind{Group: dbapi.SchemeGroupVersion.Group, Version: dbapi.SchemeGroupVersion.Version, Kind: dbapi.ResourceKindMySQL},
 	},
 	{
-		use: "perconaxtradb", aliases: []string{"prx", "perconaxtradbs"}, short: "Debug helper for PerconaXtraDB database",
+		use: dbapi.ResourceSingularPerconaXtraDB, aliases: []string{dbapi.ResourceCodePerconaXtraDB, dbapi.ResourcePluralPerconaXtraDB}, short: "Debug helper for PerconaXtraDB database",
 		gvk: schema.GroupVersionKind{Group: dbapi.SchemeGroupVersion.Group, Version: dbapi.SchemeGroupVersion.Version, Kind: dbapi.ResourceKindPerconaXtraDB},
 	},
 	{
-		use: "pgbouncer", aliases: []string{"pb", "pgbouncers"}, short: "Debug helper for PgBouncer database",
+		use: dbapi.ResourceSingularPgBouncer, aliases: []string{dbapi.ResourceCodePgBouncer, dbapi.ResourcePluralPgBouncer}, short: "Debug helper for PgBouncer database",
 		gvk: schema.GroupVersionKind{Group: dbapi.SchemeGroupVersion.Group, Version: dbapi.SchemeGroupVersion.Version, Kind: dbapi.ResourceKindPgBouncer},
 	},
 	{
-		use: "postgres", aliases: []string{"pg", "postgreses"}, short: "Debug helper for Postgres database",
+		use: dbapi.ResourceSingularPostgres, aliases: []string{dbapi.ResourceCodePostgres, dbapi.ResourcePluralPostgres}, short: "Debug helper for Postgres database",
 		gvk: schema.GroupVersionKind{Group: dbapi.SchemeGroupVersion.Group, Version: dbapi.SchemeGroupVersion.Version, Kind: dbapi.ResourceKindPostgres},
 	},
 	{
-		use: "proxysql", aliases: []string{"px", "proxysqls"}, short: "Debug helper for ProxySQL database",
+		use: dbapi.ResourceSingularProxySQL, aliases: []string{dbapi.ResourceCodeProxySQL, dbapi.ResourcePluralProxySQL}, short: "Debug helper for ProxySQL database",
 		gvk: schema.GroupVersionKind{Group: dbapi.SchemeGroupVersion.Group, Version: dbapi.SchemeGroupVersion.Version, Kind: dbapi.ResourceKindProxySQL},
 	},
 	{
-		use: "redis", aliases: []string{"rd", "redises"}, short: "Debug helper for Redis database",
+		use: dbapi.ResourceSingularRedis, aliases: []string{dbapi.ResourceCodeRedis, dbapi.ResourcePluralRedis}, short: "Debug helper for Redis database",
 		gvk: schema.GroupVersionKind{Group: dbapi.SchemeGroupVersion.Group, Version: dbapi.SchemeGroupVersion.Version, Kind: dbapi.ResourceKindRedis},
+	},
+	{
+		use: dbapi.ResourceSingularRedisSentinel, aliases: []string{dbapi.ResourceCodeRedisSentinel, dbapi.ResourcePluralRedisSentinel}, short: "Debug helper for RedisSentinel database",
+		gvk: schema.GroupVersionKind{Group: dbapi.SchemeGroupVersion.Group, Version: dbapi.SchemeGroupVersion.Version, Kind: dbapi.ResourceKindRedisSentinel},
 	},
 	// v1alpha2
 	{
-		use: "cassandra", aliases: []string{"cs", "cassandras"}, short: "Debug helper for Cassandra database",
+		use: olddbapi.ResourceSingularAerospike, aliases: []string{olddbapi.ResourceCodeAerospike, olddbapi.ResourcePluralAerospike}, short: "Debug helper for Aerospike database",
+		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindAerospike},
+	},
+	{
+		use: olddbapi.ResourceSingularCassandra, aliases: []string{olddbapi.ResourceCodeCassandra, olddbapi.ResourcePluralCassandra}, short: "Debug helper for Cassandra database",
 		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindCassandra},
 	},
 	{
-		use: "clickhouse", aliases: []string{"cl", "clickhouses"}, short: "Debug helper for ClickHouse database",
+		use: olddbapi.ResourceSingularClickHouse, aliases: []string{olddbapi.ResourceCodeClickHouse, olddbapi.ResourcePluralClickHouse}, short: "Debug helper for ClickHouse database",
 		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindClickHouse},
 	},
 	{
-		use: "druid", aliases: []string{"dr", "druids"}, short: "Debug helper for Druid database",
+		use: olddbapi.ResourceSingularDB2, aliases: []string{olddbapi.ResourceCodeDB2, olddbapi.ResourcePluralDB2}, short: "Debug helper for DB2 database",
+		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindDB2},
+	},
+	{
+		use: olddbapi.ResourceSingularDocumentDB, aliases: []string{olddbapi.ResourceCodeDocumentDB, olddbapi.ResourcePluralDocumentDB}, short: "Debug helper for DocumentDB database",
+		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindDocumentDB},
+	},
+	{
+		use: olddbapi.ResourceSingularDruid, aliases: []string{olddbapi.ResourceCodeDruid, olddbapi.ResourcePluralDruid}, short: "Debug helper for Druid database",
 		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindDruid},
 	},
 	{
-		use: "ferretdb", aliases: []string{"fr", "ferretdbs"}, short: "Debug helper for FerretDB database",
+		use: olddbapi.ResourceSingularFerretDB, aliases: []string{olddbapi.ResourceCodeFerretDB, olddbapi.ResourcePluralFerretDB}, short: "Debug helper for FerretDB database",
 		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindFerretDB},
 	},
 	{
-		use: "hazelcast", aliases: []string{"hz", "hazelcasts"}, short: "Debug helper for Hazelcast database",
+		use: olddbapi.ResourceSingularHanaDB, aliases: []string{olddbapi.ResourceCodeHanaDB, olddbapi.ResourcePluralHanaDB}, short: "Debug helper for HanaDB database",
+		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindHanaDB},
+	},
+	{
+		use: olddbapi.ResourceSingularHazelcast, aliases: []string{olddbapi.ResourceCodeHazelcast, olddbapi.ResourcePluralHazelcast}, short: "Debug helper for Hazelcast database",
 		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindHazelcast},
 	},
 	{
-		use: "ignite", aliases: []string{"ig", "ignites"}, short: "Debug helper for Ignite database",
+		use: olddbapi.ResourceSingularIgnite, aliases: []string{olddbapi.ResourceCodeIgnite, olddbapi.ResourcePluralIgnite}, short: "Debug helper for Ignite database",
 		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindIgnite},
 	},
 	{
-		use: "mssqlserver", aliases: []string{"ms", "mssqlservers"}, short: "Debug helper for MSSQLServer database",
+		use: olddbapi.ResourceSingularMilvus, aliases: []string{olddbapi.ResourceCodeMilvus, olddbapi.ResourcePluralMilvus}, short: "Debug helper for Milvus database",
+		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindMilvus},
+	},
+	{
+		use: olddbapi.ResourceSingularMSSQLServer, aliases: []string{olddbapi.ResourceCodeMSSQLServer, olddbapi.ResourcePluralMSSQLServer}, short: "Debug helper for MSSQLServer database",
 		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindMSSQLServer},
 	},
 	{
-		use: "oracle", aliases: []string{"or", "oracles"}, short: "Debug helper for Oracle database",
+		use: olddbapi.ResourceSingularNeo4j, aliases: []string{olddbapi.ResourceCodeNeo4j, olddbapi.ResourcePluralNeo4j}, short: "Debug helper for Neo4j database",
+		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindNeo4j},
+	},
+	{
+		use: olddbapi.ResourceSingularOracle, aliases: []string{olddbapi.ResourceCodeOracle, olddbapi.ResourcePluralOracle}, short: "Debug helper for Oracle database",
 		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindOracle},
 	},
 	{
-		use: "pgpool", aliases: []string{"pp", "pgpools"}, short: "Debug helper for Pgpool database",
+		use: olddbapi.ResourceSingularPgpool, aliases: []string{olddbapi.ResourceCodePgpool, olddbapi.ResourcePluralPgpool}, short: "Debug helper for Pgpool database",
 		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindPgpool},
 	},
 	{
-		use: "rabbitmq", aliases: []string{"rm", "rabbitmqs"}, short: "Debug helper for Rabbitmq database",
+		use: olddbapi.ResourceSingularQdrant, aliases: []string{olddbapi.ResourceCodeQdrant, olddbapi.ResourcePluralQdrant}, short: "Debug helper for Qdrant database",
+		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindQdrant},
+	},
+	{
+		use: olddbapi.ResourceSingularRabbitmq, aliases: []string{olddbapi.ResourceCodeRabbitmq, olddbapi.ResourcePluralRabbitmq}, short: "Debug helper for RabbitMQ database",
 		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindRabbitmq},
 	},
 	{
-		use: "singlestore", aliases: []string{"sdb", "singlestores"}, short: "Debug helper for Singlestore database",
+		use: olddbapi.ResourceSingularSinglestore, aliases: []string{olddbapi.ResourceCodeSinglestore, olddbapi.ResourcePluralSinglestore}, short: "Debug helper for Singlestore database",
 		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindSinglestore},
 	},
 	{
-		use: "solr", aliases: []string{"sl", "solrs"}, short: "Debug helper for Solr database",
+		use: olddbapi.ResourceSingularSolr, aliases: []string{olddbapi.ResourceCodeSolr, olddbapi.ResourcePluralSolr}, short: "Debug helper for Solr database",
 		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindSolr},
 	},
 	{
-		use: "zookeeper", aliases: []string{"zk", "zookeepers"}, short: "Debug helper for Zookeeper database",
+		use: olddbapi.ResourceSingularWeaviate, aliases: []string{olddbapi.ResourceCodeWeaviate, olddbapi.ResourcePluralWeaviate}, short: "Debug helper for Weaviate database",
+		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindWeaviate},
+	},
+	{
+		use: olddbapi.ResourceSingularZooKeeper, aliases: []string{olddbapi.ResourceCodeZooKeeper, olddbapi.ResourcePluralZooKeeper}, short: "Debug helper for ZooKeeper database",
 		gvk: schema.GroupVersionKind{Group: olddbapi.SchemeGroupVersion.Group, Version: olddbapi.SchemeGroupVersion.Version, Kind: olddbapi.ResourceKindZooKeeper},
 	},
 }
